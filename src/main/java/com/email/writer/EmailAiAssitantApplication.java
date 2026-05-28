77686203a8dc7cddd62bfd622f581a021aa44e63
NishanthiniB <nishasuresh1305@gmail.com>
2026-05-28 11:00:44 +0530

    EmailAIProject_Process1

---
+++ src/main/java/com/email/writer/EmailAiAssitantApplication.java
@@ -0,0 +1,13 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAiAssitantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailAiAssitantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/email/writer/EmailAiAssitantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAiAssitantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
